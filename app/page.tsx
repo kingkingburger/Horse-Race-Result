936786bad2184c3f552c638ab682b2a807d64ace
minho <dnjsalsgh123@gmail.com>
2023-07-19 21:16:49 +0900

    [ADD]: using NextUi

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
+"use client";
 import Example from "@/app/custom/page";
 import React from "react";
 import Link from "next/link";
+import { Button } from "@nextui-org/react";
 
 export default function Page() {
   return (
@@ -12,23 +14,7 @@ export default function Page() {
       <Link href="/about">
         <div>about</div>
       </Link>
-      <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-        <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
-          <blockquote>
-            <p className="text-lg font-medium">
-              “Tailwind CSS is the only framework that I've seen scale on large
-              teams. It’s easy to customize, adapts to any design, and the build
-              size is tiny.”
-            </p>
-          </blockquote>
-          <figcaption className="font-medium">
-            <div className="text-sky-500 dark:text-sky-400">Sarah Dayan</div>
-            <div className="text-slate-700 dark:text-slate-500">
-              Staff Engineer, Algolia
-            </div>
-          </figcaption>
-        </div>
-      </figure>
+      <Button>Click me</Button>
     </div>
   );
 }
