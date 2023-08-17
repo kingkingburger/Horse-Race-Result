43faccbadf4cc51b269f570ce6b1a5909fc66e3b
minho <dnjsalsgh123@gmail.com>
2023-08-18 06:50:35 +0900

    [fix]: matching title with content

--- app/read/[id]/page.tsx
+++ app/read/[id]/page.tsx
@@ -1,16 +1,25 @@
 import { PropsWithChildren, PropsWithRef } from "react";
 
+interface mongoDBresponse {
+  _id: number;
+  testId: number;
+  title: string;
+  content: string;
+}
+
 export default async function Read(props: PropsWithRef<any>) {
   console.log("props = ", props);
+
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_MONGO_API_URL}/api/test?testId=${props.params.id}`,
     { cache: "no-store" }
   );
-  const topic = await response.json();
+  const topic = (await response.json()) as mongoDBresponse;
+  console.log("topic = ", topic);
   return (
     <>
       <h2>{topic.title}</h2>
-      {topic.body}
+      {topic.content}
     </>
   );
 }
