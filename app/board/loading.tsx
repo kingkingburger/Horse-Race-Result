70674cc0142984238a4a642a265daeaf953438d9
kingkingburger <dnjsalsgh123@gmail.com>
2023-08-30 17:23:24 +0900

    [Update]: implement loading page at animating circle spin

--- app/board/loading.tsx
+++ app/board/loading.tsx
@@ -1,19 +1,26 @@
+import styles from '../loading.module.css'
+
 export default function Loading() {
     return (
-        <div>
-            <p className="text-3xl"> Posts</p>
-            <ul className="list-disc pl-6 mt-6 space-y-2">
-                {Array(20).map((i) => (
-                    <li key={i}>
-                        <span className="inline-block h-5 animate-pulse"
-                              style={{
-                                  animationDelay: `${i * 0.05}s`,
-                                  animationDuration: '1s'
-                              }}
-                        />
-                    </li>
-                ))}
-            </ul>
+        // <div>
+        //     <p className="flex justify-center mt-3 w-full sm:w-1/2 text-3xl"> Posts</p>
+        //     <ul className="flex justify-center mt-3 w-full sm:w-1/2 list-disc pl-6 mt-6 space-y-2">
+        //         {Array(20).map((i) => (
+        //             <li key={i}>
+        //                 <span className="inline-block h-5 animate-pulse"
+        //                       style={{
+        //                           animationDelay: `${i * 0.05}s`,
+        //                           animationDuration: '1s'
+        //                       }}
+        //                 />
+        //             </li>
+        //         ))}
+        //     </ul>
+        // </div>
+        <div className={styles.container}>
+            <div className={styles.loadingContainer}>
+                <div className={styles.spinner}></div>
+            </div>
         </div>
     )
 }
--- app/loading.tsx
+++ app/loading.tsx
@@ -1,8 +1,14 @@
+import styles from "@/app/loading.module.css";
+
 export default function loading () {
     return(
-        <div className="flex flex-wrap justify-center space-x-2 min-w-[400px]">
-            로딩중입니다.
+        // <div className="flex flex-wrap justify-center space-x-2 min-w-[400px]">
+        //     로딩중입니다.
+        // </div>
+        <div className={styles.container}>
+            <div className={styles.loadingContainer}>
+                <div className={styles.spinner}></div>
+            </div>
         </div>
     )
-    // You can add any UI inside Loading, including a Skeleton.
 }
